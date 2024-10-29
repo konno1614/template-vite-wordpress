43405ee95a09f1509f7023075534a2e41e9230dd
konno1614 <konno1614@gmail.com>
2024-10-29 12:05:49 +0900

    add /plugin

---
+++ wp-content/themes/template-vite-wordpress/footer.php
@@ -0,0 +1,13 @@
+<?php ?>
+</main>
+
+<footer class="l-footer">
+    <p>footer</p>
+    <small class="l-footer__copy">&copy;<?php echo date('Y'); ?> CCDN Co., Ltd.</small>
+</footer>
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
---
+++ wp-content/themes/template-vite-wordpress/front-page.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<div class="p-index">
+    <h1>Frontpage</h1>
+    <p>This is the frontpage</p>
+</div>
+
+<?php get_footer(); ?>
