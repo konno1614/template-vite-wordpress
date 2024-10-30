f7180a97ac229e0e0cc3c744d25b9b434c956763
konno1614 <konno1614@gmail.com>
2024-10-30 15:16:37 +0900

    update

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,22 +5,19 @@ import { fileURLToPath } from "node:url";
 
 const root = resolve(__dirname, "src");
 
-// WordPress用ビルドのinput設定。WordPress用にはhtmlファイルは不要なため、scssとtsのみをビルド対象にする
 const inputsForWordPress = {
     style: resolve(root, "/assets/style/style.scss"),
-    // 動的にファイルを取得する @see https://rollupjs.org/configuration-options/#input
     ...Object.fromEntries(
         globSync("src/assets/js/*.ts").map((file) => [
-        relative(
-            "src/assets/js",
-            file.slice(0, file.length - extname(file).length),
-        ),
-        fileURLToPath(new URL(file, import.meta.url)),
+            relative(
+                "src/assets/js",
+                file.slice(0, file.length - extname(file).length),
+            ),
+            fileURLToPath(new URL(file, import.meta.url))
         ]),
     ),
 };
 
-// 静的開発用のinput設定。静的資材用にはhtmlファイルを経由してscss,tsなどをビルドする
 const inputsForStatic = {
     style: resolve(root, "/assets/style/style.scss"),
     ...Object.fromEntries(
