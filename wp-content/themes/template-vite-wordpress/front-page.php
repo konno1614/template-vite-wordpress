c6cb8da0a2878c7d0d055c39ed6ccacc39ed7afc
konno1614 <konno1614@gmail.com>
2024-11-07 19:33:24 +0900

    update

--- wp-content/themes/template-vite-wordpress/front-page.php
+++ wp-content/themes/template-vite-wordpress/front-page.php
@@ -1,12 +1,14 @@
 <?php get_header(); ?>
 
 <div class="p-index">
-    <h2 class="p-index__title">index.html</h2>
-    <p class="p-index__text">Text Text Text Text Text Text Text Text Text</p>
-    <div class="p-index__image">
-        <img src="<?php echo get_template_directory_uri();?>/assets/images/index/cat-html.png" width="512" height="512" alt="" />
+    <div class="l-inner">
+        <h2 class="p-index__title">index.html</h2>
+        <p class="p-index__text">Text Text Text Text Text Text Text Text Text</p>
+        <div class="p-index__image">
+            <img src="<?php echo get_template_directory_uri();?>/assets/images/index/cat-html.png" width="512" height="512" alt="" />
+        </div>
+        <div class="p-index__bg"></div>
     </div>
-    <div class="p-index__bg"></div>
 </div>
 
 <?php get_footer(); ?>
