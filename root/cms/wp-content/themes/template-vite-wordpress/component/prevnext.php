82df2c974dab391042e65bc39e885c15c0f0fa57
konno1614 <konno1614@gmail.com>
2024-11-19 15:11:20 +0900

    update

--- root/cms/wp-content/themes/template-vite-wordpress/component/prevnext.php
+++ root/cms/wp-content/themes/template-vite-wordpress/component/prevnext.php
@@ -10,24 +10,6 @@
         <?php if (!empty($next_link)): ?>
             <a href="<?php echo esc_url($next_link); ?>">前の記事</a>
         <?php else: ?>
-            <?php
-            $args = array(
-                'post_type' => 'post',
-                'posts_per_page' => 1,
-                'order' => 'DESC',
-                'orderby' => 'date',
-            );
-            $first_post_query = new WP_Query($args);
-            if ($first_post_query->have_posts()) {
-                while ($first_post_query->have_posts()) {
-                    $first_post_query->the_post();
-                    $first_post_title = get_the_title();
-                    $first_post_link = get_permalink();
-                    echo "<a href='$first_post_link'>Next</a>";
-                }
-                wp_reset_postdata();
-            }
-            ?>
         <?php endif; ?>
     </li>
     <li class="index"><a href="<?php echo esc_url(home_url('/')); ?>news/"></a></li>
@@ -42,24 +24,6 @@
         <?php if (!empty($prev_link)): ?>
             <a href="<?php echo esc_url($prev_link); ?>">次の記事</a>
         <?php else: ?>
-            <?php
-            $args = array(
-                'post_type' => 'post',
-                'posts_per_page' => 1,
-                'order' => 'ASC',
-                'orderby' => 'date',
-            );
-            $first_post_query = new WP_Query($args);
-            if ($first_post_query->have_posts()) {
-                while ($first_post_query->have_posts()) {
-                    $first_post_query->the_post();
-                    $first_post_title = get_the_title();
-                    $first_post_link = get_permalink();
-                    echo "<a href='$first_post_link'>Prev</a>";
-                }
-                wp_reset_postdata();
-            }
-            ?>
         <?php endif; ?>
     </li>
 </ul>
